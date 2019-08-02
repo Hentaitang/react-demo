64eddd38609dbc8f9dec7a869404a1aa5770b207
Hentaitang <mz374427166@gmail.com>
2019-08-02 23:34:27 +0800

    react Context

--- react-redux-1/src/App.js
+++ react-redux-1/src/App.js
@@ -1,38 +1,85 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+/*
+ * use redux
+ */
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//   }
+//   add1() {
+//     this.props.store.dispatch({ type: 'add', payload: 1 });
+//   }
+//   add2() {
+//     this.props.store.dispatch({ type: 'add', payload: 2 });
+//   }
+//   addIf() {
+//     if (this.props.store.getState() % 2 === 1) {
+//       this.props.store.dispatch({ type: 'add', payload: 1 });
+//     }
+//   }
+//   addAsync() {
+//     setTimeout(() => {
+//       this.props.store.dispatch({ type: 'add', payload: 1 });
+//     }, 2000);
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         你点击了{this.props.value}次
+//         <div>
+//           <button onClick={() => this.add1()}>+1</button>
+//           <button onClick={() => this.add2()}>+2</button>
+//           <button onClick={() => this.addIf()}>如果是单数就+1</button>
+//           <button onClick={() => this.addAsync()}>两秒后+1</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 class App extends React.Component {
-  constructor() {
-    super();
-  }
-  add1() {
-    this.props.store.dispatch({ type: 'add', payload: 1 });
-  }
-  add2() {
-    this.props.store.dispatch({ type: 'add', payload: 2 });
-  }
-  addIf() {
-    if (this.props.store.getState() % 2 === 1) {
-      this.props.store.dispatch({ type: 'add', payload: 1 });
-    }
-  }
-  addAsync() {
-    setTimeout(() => {
-      this.props.store.dispatch({ type: 'add', payload: 1 });
-    }, 2000);
-  }
   render() {
     return (
       <div className="App">
-        你点击了{this.props.value}次
+        你点击了{this.props.n}次
         <div>
-          <button onClick={() => this.add1()}>+1</button>
-          <button onClick={() => this.add2()}>+2</button>
-          <button onClick={() => this.addIf()}>如果是单数就+1</button>
-          <button onClick={() => this.addAsync()}>两秒后+1</button>
+          <button onClick={() => this.props.add1()}>+1</button>
+          <button onClick={() => this.props.add2()}>+2</button>
+          <button>如果是单数就+1</button>
+          <button>两秒后+1</button>
+        </div>
+        <div>
+          <a href="https://codesandbox.io/s/heuristic-fermi-vnowd">
+            React Context
+          </a>
+        </div>
+        <div>
+          <a href="https://codesandbox.io/s/cool-browser-5i794"> React-redux</a>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+function getPartialState(state) {
+  return {
+    n: state.n
+  };
+}
+
+const actionCreator = {
+  add1: () => {
+    return { type: 'add', payload: 1 };
+  },
+  add2: () => {
+    return { type: 'add', payload: 2 };
+  }
+};
+
+// export default App;
+export default connect(
+  getPartialState,
+  actionCreator
+)(App);
--- react-redux-1/src/index.js
+++ react-redux-1/src/index.js
@@ -4,27 +4,38 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const reducer = (state, action) => {
-  if (state === undefined) return 0;
+  if (state === undefined) return { n: 0 };
   switch (action.type) {
     case 'add':
-      return state + action.payload;
+      return { n: state.n + action.payload };
     default:
       return state;
   }
 };
 const store = createStore(reducer);
 
-function render() {
-  ReactDOM.render(
-    <App value={store.getState()} store={store} />,
-    document.getElementById('root')
-  );
-}
+/*
+ * use redux
+ */
+// function render() {
+//   ReactDOM.render(
+//     <App value={store.getState()} store={store} />,
+//     document.getElementById('root')
+//   );
+// }
 
-store.subscribe(render);
-render();
+// store.subscribe(render);
+// render();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
